28a92183a386ccf1b9c0d7ab570f270cf944ff1e
naveenvivek <modernnaveen@gmail.com>
2023-09-29 10:49:02 +0800

    first commit
    first commit

---
+++ src/test/java/com/coding/stocks/StocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
