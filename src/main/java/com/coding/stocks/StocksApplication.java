2f33f3611e2d9b2ed6a55a4589824d44f63f1acf
naveenvivek <modernnaveen@gmail.com>
2023-09-29 10:58:14 +0800

    commit

--- src/main/java/com/coding/stocks/StocksApplication.java
+++ src/main/java/com/coding/stocks/StocksApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StocksApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello");
+
         SpringApplication.run(StocksApplication.class, args);
     }
 
